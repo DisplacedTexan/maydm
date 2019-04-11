afdee81b9026e1c09bf313129afa113520442170
Ashlie <ashliecrooks@gmail.com>
2019-04-11 14:02:24 -0500

    final script plus starter script

--- web-dev/pokedex/script.js
+++ web-dev/pokedex/script.js
@@ -1,10 +1,3 @@
-// fetch("https://pokeapi.co/api/v2/pokemon/1")
-//   .then(res => res.json())
-//   .then(pokemon => {
-//     console.log(pokemon);
-//   })
-//   .catch(err => console.error(err));
-
 var input = document.getElementById("pokemonToGet");
 input.addEventListener("keydown", function(e) {
   if (e.keyCode === 13) {
@@ -14,20 +7,28 @@ input.addEventListener("keydown", function(e) {
 
 function getPokemon() {
   var pokemon = document.getElementById("pokemonToGet").value;
+
+  fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
+    .then(res => res.json())
+    .then(pokemon => showPokemon(pokemon))
+    .catch(err => console.log(err));
+}
+
+function showPokemon(pokemon) {
   var name = document.getElementById("PokemonName");
   var image = document.getElementById("PokemonImage");
   var height = document.getElementById("PokemonHeight");
   var weight = document.getElementById("PokemonWeight");
   var type = document.getElementById("PokemonList");
 
-  fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
-    .then(res => res.json())
-    //.then(pokemon => console.log(pokemon))
-    .then(pokemon => {
-      name.innerHTML = pokemon.name;
-      height.innerHTML = pokemon.height;
-      weight.innerHTML = pokemon.weight;
-      image.src = pokemon.sprites.front_default;
-    })
-    .catch(err => console.log(err));
+  var typesList = [];
+  pokemon.types.forEach(function(type) {
+    typesList.push(`<li>${type.type.name}</li>`);
+  });
+
+  name.innerHTML = pokemon.name;
+  height.innerHTML = pokemon.height;
+  weight.innerHTML = pokemon.weight;
+  image.src = pokemon.sprites.front_default;
+  type.innerHTML = typesList ? typesList.join("") : "";
 }
