497089793d7b811a21b7a7ec0002c4ef6b1b373f
Ashlie <ashliecrooks@gmail.com>
2019-04-11 13:31:12 -0500

    finished script

--- web-dev/pokedex/script.js
+++ web-dev/pokedex/script.js
@@ -5,11 +5,29 @@
 //   })
 //   .catch(err => console.error(err));
 
+var input = document.getElementById("pokemonToGet");
+input.addEventListener("keydown", function(e) {
+  if (e.keyCode === 13) {
+    getPokemon();
+  }
+});
+
 function getPokemon() {
   var pokemon = document.getElementById("pokemonToGet").value;
+  var name = document.getElementById("PokemonName");
+  var image = document.getElementById("PokemonImage");
+  var height = document.getElementById("PokemonHeight");
+  var weight = document.getElementById("PokemonWeight");
+  var type = document.getElementById("PokemonList");
 
   fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
     .then(res => res.json())
-    .then(pokemon => console.log(pokemon))
+    //.then(pokemon => console.log(pokemon))
+    .then(pokemon => {
+      name.innerHTML = pokemon.name;
+      height.innerHTML = pokemon.height;
+      weight.innerHTML = pokemon.weight;
+      image.src = pokemon.sprites.front_default;
+    })
     .catch(err => console.log(err));
 }
